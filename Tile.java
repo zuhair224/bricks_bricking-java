79967095c912b0db110b3cc30f36af06b0e730be
Zuhair -Alzekri <75549570+zuhair224@users.noreply.github.com>
2021-04-29 21:23:25 +0200

    Create Tile.java

---
+++ Tile.java
@@ -0,0 +1,32 @@
+package sample;
+
+public class Tile {
+
+
+    private int value ;
+    private String color;
+
+    public Tile() {
+    }
+
+    public Tile(int value, String color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
